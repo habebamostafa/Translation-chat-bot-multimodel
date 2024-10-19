adb1cbba80407374f82fd2d2094b63c1790fdd1e
Habeba Mostafa <127840000+habebamostafa@users.noreply.github.com>
2024-10-19 23:28:25 +0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -43,7 +43,7 @@ def load_translation_model():
 
 
 def translate_sentence(english_sentence):
-    model = pipeline("translation_en_to_ar", model='model')
+    model = pipeline("translation_en_to_ar", model='Helsinki-NLP/opus-mt-en-ar')
     translate_sentence = model(english_sentence)
     translated = translate_sentence[0]['translation_text']
     return translated
