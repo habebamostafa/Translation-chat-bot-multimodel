6836b9334082b7109fe694ea4d5b9e8451f6ad5e
Habeba Mostafa <127840000+habebamostafa@users.noreply.github.com>
2024-10-19 22:18:16 +0300

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -29,7 +29,7 @@ def read_pdf(file):
 def load_image_to_text_model():
     try:
         model_path = os.path.join(os.path.dirname(__file__), 'cnn_model.h5')
-        return tf.keras.models.load_model(model_path, custom_objects={'CustomLayer': CustomLayer})
+        return tf.keras.models.load_model(model_path)
     except OSError as e:
         st.error(f"Error loading model: {e}")
         return None
